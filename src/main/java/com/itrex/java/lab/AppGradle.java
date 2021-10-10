c1377d75168beaea3c224b0ec59d6fa765e813eb
Sergei-JD <sergshlyazhko@gmail.com>
2021-10-10 19:50:12 +0300

    Simple Gradle Project

---
+++ src/main/java/com/itrex/java/lab/AppGradle.java
@@ -0,0 +1,11 @@
+package com.itrex.java.lab;
+
+public class AppGradle {
+
+    public static void main(String[] args) {
+        System.out.println("\n==============start============\n");
+        System.out.println( "Hello World From Gradle!" );
+        System.out.println("\n===============end=============\n");
+    }
+
+}
